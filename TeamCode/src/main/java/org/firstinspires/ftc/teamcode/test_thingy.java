bb1b480340dad31e3f97488ebb3bc04bbf48772d
Caleb <calebsurprenant@gmail.com>
2024-10-25 17:04:48 -0500

    Added servos and extending arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test_thingy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test_thingy.java
@@ -3,10 +3,13 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
 import java.lang.Math;
 
-@TeleOp(name = "TeleOp (no arm)")
+@TeleOp(name = "TeleOp")
 public class test_thingy extends OpMode {
     //name motors
     DcMotor motorFL;
@@ -14,6 +17,17 @@ public class test_thingy extends OpMode {
     DcMotor motorBL;
     DcMotor motorBR;
     DcMotor ArmMotor;
+    DcMotor ExtendArm;
+    CRServo grabber;
+    Servo wrist;
+    //name servos
+
+
+    double desVal = 0;
+    double wanVal = 0;
+
+    double prevErr = 0;
+    double prevExt = 0;
     //initialize
     @Override
     public void init() {
@@ -22,6 +36,9 @@ public class test_thingy extends OpMode {
         motorBL = hardwareMap.get(DcMotor.class, "motorBL");
         motorBR = hardwareMap.get(DcMotor.class, "motorBR");
         ArmMotor = hardwareMap.get(DcMotor.class, "ArmMotor");
+        ExtendArm = hardwareMap.get(DcMotor.class, "ExtendArm");
+        grabber = hardwareMap.get(CRServo.class, "grabber");
+        wrist = hardwareMap.get(Servo.class, "wrist");
 
         telemetry.addData("Hardware", "much gud");
     }
@@ -33,6 +50,7 @@ public class test_thingy extends OpMode {
             float y = gamepad1.left_stick_y;
             float x = gamepad1.left_stick_x;
             float t = gamepad1.right_stick_x;
+            float ea = gamepad2.right_stick_y;
             ArmMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
             //up and down, left right, and turning
@@ -40,33 +58,87 @@ public class test_thingy extends OpMode {
             motorFR.setPower(-y - x - t);
             motorBL.setPower(y + x - t);
             motorBR.setPower(-y + x - t);
+            ArmMotor.setPower(ea);
 
-            //Arm up and down (WIP)
 
-            boolean raise_arm = false;
+            //Arm up define arm values
+            double upVal = 1526;
+            double dwnVal = 0;
+            double outVal = 400;
+            double inVal = 0;
+            double enVal = ArmMotor.getCurrentPosition();
+            double lenVal = ExtendArm.getCurrentPosition();
 
-            // If triangle button is pressed, increase arm motor position
+            // If Y button is pressed, increase arm motor position
             if(gamepad2.triangle) {
-                encoder(2);
+                desVal = upVal;
+            }
+            // If A button is pressed, increase arm motor position
+            if(gamepad2.cross) {
+                desVal = dwnVal;
             }
 
-            // If arm motor has reached target position, stop movement
-            if (ArmMotor.getCurrentPosition() >= ArmMotor. getTargetPosition()) {
-                ArmMotor.setPower(0);
+            // If X button held, make grabber pickup
+            if(gamepad2.square){
+                grabber.setPower(-2);
             }
-            telemetry.addData("Motor ticks:", ArmMotor.getCurrentPosition());
+
+            //If B button held, make grabber spit out
+            else if(gamepad2.circle){
+                grabber.setPower(+2);
+            }
+
+            else{
+                grabber.setPower(0);
+            }
+
+            if (gamepad2.dpad_left){
+                wrist.setPosition(0.43);
+            }
+
+            if (gamepad2.dpad_right) {
+                wrist.setPosition(0.75);
+            }
+
+            if(gamepad2.dpad_up){
+                wanVal = outVal;
+            }
+
+            if(gamepad2.dpad_down){
+                wanVal = inVal;
+            }
+
+            double extend = wanVal - lenVal;
+            double error = desVal - enVal;
+
+            //Arm up/down stuff
+            double P = 0.005;
+            double D = 0.005;
+
+            double errD = error - prevErr;
+            prevErr = error;
+            double errP = error;
+
+
+
+
+            double Imp = (P * errP) + (D * errD);
+            ArmMotor.setPower(Imp);
+            enVal = ArmMotor.getCurrentPosition();
+            telemetry.addData("Arm Value", enVal);
+
+            //Arm in/out stuff
+            double p = 0.005;
+            double d = 0.005;
+
+            double extD = extend - prevExt;
+            prevExt = extend;
+            double extP = extend;
+
+            double move = (p * extP) + (d * extD);
+            ExtendArm.setPower(move);
+            lenVal = ExtendArm.getCurrentPosition();
+            telemetry.addData("Extend Value", lenVal);
 
         }
-
-        public void encoder(int turnage)
-        {
-            // Number of ticks associated with the motor
-            double ticks = 537.7;
-            // Target position is current position + (ticks/rotation * number of rotations)
-            double newTarget = ArmMotor.getCurrentPosition() + (ticks * turnage);
-
-            // Set arm motor target position and power
-            ArmMotor.setTargetPosition((int) newTarget);
-            ArmMotor.setPower(0.5);
         }
-}
